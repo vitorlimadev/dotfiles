d89ea02b9708fae42b91c56861224ec8676aa27c
vitorlimadev <vitorlimadev@outlook.com>
2022-07-14 19:48:15 -0300

    put plugins on a different file

---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,31 @@
+require('packer').startup(function(use)
+  -- Package manager
+  use 'wbthomason/packer.nvim'
+  -- LSP Client tools
+  use 'neovim/nvim-lspconfig' -- LSP Configuration
+  use 'nvim-treesitter/nvim-treesitter' -- Improves LSP functionalities
+  use {
+    'hrsh7th/nvim-cmp', -- Autocompletion plugin
+    requires = { {'hrsh7th/cmp-nvim-lsp'} }
+  }
+  -- Git interface
+  use 'tpope/vim-fugitive'
+  -- Telescope (File search, search for word occourencies in a project)
+  use {
+    'nvim-telescope/telescope.nvim',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
+  -- File tree
+  use {
+    'kyazdani42/nvim-tree.lua',
+    config = function() require'nvim-tree'.setup {} end
+  }
+  -- Comment a selection of text
+  use 'tpope/vim-commentary'
+  -- Surround words/selections with tags, "", '' etc...
+  use 'tpope/vim-surround'
+  -- Fixes buffer deletion to not delete windows
+  use 'moll/vim-bbye'
+  -- Icons
+  use "kyazdani42/nvim-web-devicons"
+end)
